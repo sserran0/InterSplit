6d133cff5c198526319162ea3470d0cc903d7e5c
Santi <santiagoserrano192@gmail.com>
2026-04-30 00:00:41 -0600

    Main.go tweaks

---
+++ server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+    "net/http"
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
+    "github.com/joho/godotenv"
+)
+func main() {
+	godotenv.Load()
+    db := connectDB()   
+    runMigrations(db)
+
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Use(middleware.Recoverer)
+    r.Use(corsMiddleware)  
+
+    r.Route("/api", func(r chi.Router) {
+        r.Post("/auth/register", handleRegister(db))
+        r.Post("/auth/login",    handleLogin(db))
+
+        r.Group(func(r chi.Router) {
+            r.Use(jwtMiddleware)  // protected routes
+            r.Get("/groups",             handleGetGroups(db))
+            r.Post("/groups",            handleCreateGroup(db))
+            r.Post("/groups/{id}/join",  handleJoinGroup(db))
+            r.Post("/expenses",          handleCreateExpense(db))
+            r.Get("/groups/{id}/expenses", handleGetExpenses(db))
+            r.Get("/rates",              handleGetRates(db))
+        })
+    })
+	port := os.Getenv("PORT")
+    http.ListenAndServe(":"+port, r)
+}
