2db93ca3eefe9ff218aa4c4e79b372a561e2b56e
Santi <santiagoserrano192@gmail.com>
2026-04-29 22:25:42 -0600

    Group Tables

---
+++ server/db/migrations/create_groups.sql
@@ -0,0 +1,13 @@
+CREATE TABLE groups (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    created_by UUID REFERENCES users(id),
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE group_members (
+    group_ud UUID REFERENCES groups(id) ON DELETE CASCADE, 
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE, --deletes rows of members for group deletion 
+    joined_at TIMESTAMPTZ DEFAULT NOW(),
+    RIMARY KEY (group_id, user_id)
+);
