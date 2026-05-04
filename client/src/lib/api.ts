a626e6a6bd0638b90adf7cc6cd8c562675c933d5
Santi <santiagoserrano192@gmail.com>
2026-05-04 17:23:21 -0600

    axios api

---
+++ client/src/lib/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { useAuthStore } from '../store/auth'
+
+const api = axios.create({
+    baseURL: '/api'
+})
+
+api.interceptors.request.use((config) => {
+    const token = useAuthStore.getState().token
+    if (token){
+        config.headers.Authorization = `Bearer: ${token}`
+    }
+    return config
+
+})
+
+export default api
